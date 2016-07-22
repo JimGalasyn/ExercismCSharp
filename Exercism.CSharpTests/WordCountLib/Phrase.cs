070074d5634256d29e6fa7034d8014ff9f11fcff
Jim Galasyn <jgalasyn@microsoft.com>
2016-07-22 14:27:35 -0700

    Added comments.

--- Exercism.CSharpTests/WordCountLib/Phrase.cs
+++ Exercism.CSharpTests/WordCountLib/Phrase.cs
@@ -1,25 +1,46 @@
-﻿using System;
+﻿// Solution to exercism problem: charp / word-count
+// http://exercism.io/exercises/csharp/word-count/readme
+// Copyright (c) 2016 James P. Galasyn
+// This project is licensed under the terms of the MIT license.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace WordCountLib
 {
+    /// <summary>
+    /// Contains static methods for splitting a phrase into its component words.
+    /// </summary>
     public class Phrase
     {
+        /// <summary>
+        /// Counts the number of times eac word appears in a phrase.
+        /// </summary>
+        /// <param name="phrase">The phrase to analyze.</param>
+        /// <returns>A dictionary of word counts.</returns>
         public static Dictionary<string, int> WordCount(string phrase)
         {
+            // Create the dictionary that holds the word counts.
             Dictionary<string, int> wordCounts = new Dictionary<string, int>();
 
+            // Remove the non-alphanumeric characters. Spaces and commas are retained.
             var phraseScrubbed = FilterNonAlpha(phrase);
 
+            // Split the phrase into component words.
             var words = phraseScrubbed.Split(new[] {' ', ',' });
+
+            // The previous Split operation produces spurious empty words, so remove these.
             var wordsScrubbed = words.Where(w => !String.IsNullOrWhiteSpace(w));
 
+            // Iterate over the word list and add words to the dictionary.
             foreach (var word in wordsScrubbed)
             {
+                // Words may have spurious apostrophes, so remove these.
                 string scrubbedWord = ScrubWord(word);
                 if (scrubbedWord != String.Empty)
                 {
+                    // Add the word and its count to the dictionary.
                     if (wordCounts.ContainsKey(scrubbedWord))
                     {
                         wordCounts[scrubbedWord]++;
@@ -34,6 +55,12 @@ namespace WordCountLib
             return wordCounts;
         }
 
+        /// <summary>
+        /// Removes spurious apostrophes from the specified word.
+        /// </summary>
+        /// <param name="word">The word to scrub.</param>
+        /// <returns>The scrubbed word.</returns>
+        /// <remarks>This method also converts the word to lower case.</remarks>
         private static string ScrubWord(string word)
         {
             string wordLowerCase = word.ToLower();
@@ -55,6 +82,15 @@ namespace WordCountLib
             return wordScrubbed;
         }
 
+        /// <summary>
+        /// Filters non-alphanumeric characters from the specified string.
+        /// </summary>
+        /// <param name="str">The string to filter.</param>
+        /// <returns>The filtered string.</returns>
+        /// <remarks>Apostrophes and commas are retained.
+        /// Adapted from: How do I remove all non alphanumeric characters from a string except dash?
+        /// http://stackoverflow.com/questions/3210393/how-do-i-remove-all-non-alphanumeric-characters-from-a-string-except-dash
+        /// </remarks>
         private static string FilterNonAlpha(string str)
         {
             char[] charArray = str.ToCharArray();
